8372ed631e5a5dc31f6c9c562d38a1f4cce27e42
Durga Bhavani <48315819+DurgaBhavaniVankini@users.noreply.github.com>
2019-03-22 15:02:54 +0530

    Update Item.py

--- Item.py
+++ Item.py
@@ -1,19 +1,25 @@
 from sqlalchemy import create_engine, asc
 from sqlalchemy.orm import sessionmaker
 from database_setup import Base, FlightPartner, FlightInfo, User
-from flask import session as login_session
 import random
 import string
 from oauth2client.client import flow_from_clientsecrets
 from oauth2client.client import FlowExchangeError
 import httplib2
 import json
-from flask import make_response
 import requests
 from functools import wraps
-from flask import Flask, render_template, \
-                  url_for, request, redirect,\
-                  flash, jsonify
+from flask import (
+    Flask, 
+    render_template,
+    url_for, 
+    request, 
+    redirect,
+    flash, 
+    jsonify,
+    make_response,
+    session as login_session
+)
 
 app = Flask(__name__)
 
